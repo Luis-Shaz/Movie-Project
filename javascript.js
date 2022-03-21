83529baf94abda163808ffdd339db908fecf9d55
Shaz Soto-Munet <shaz.soto.munet@gmail.com>
2022-03-21 17:00:15 -0500

    Add forEach loop for the movie array.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,19 @@
-$.ajax('db.json').done(function(data){
-    console.log(data);
-});
+const movieContainer = $("#movie-container")
+
+const movies = fetch('db.json')
+    .then(response => response.json())
+    .then(response => {
+        response.movies.forEach(function(element){
+            movieContainer.html(
+                `<div> ${element.title} </div>`);
+        })
+
+    })
+
+// function addMovie(){
+//     e.preventDefault()
+//     console.log("Add movie button works!")
+// }
+
+// $("#add-movie-btn").click(addMovie);
 
