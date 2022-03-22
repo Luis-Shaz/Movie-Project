a51dfb1ba73e44c84dc6a469daabf2649660e12f
Shaz Soto-Munet <shaz.soto.munet@gmail.com>
2022-03-22 12:29:35 -0500

    Add variables for movie title and rating input; add loop to sort thru radio buttons and return value to a variable.

--- javascript.js
+++ javascript.js
@@ -1,21 +1,55 @@
-const movies = fetch('db.json')
-    .then(response => response.json())
-    .then(response => {
-        let html = "";
-        response.movies.forEach(function(element){
-            html +=
-                "<div>" + element.title + "</div>" +
-                "<div>" + element.rating + "</div>";
-            $("#movie-container").html(html);
+
+function displayMovies() {
+    fetch('https://chestnut-endurable-ixia.glitch.me/movies')
+        .then(response => response.json())
+        // .then(console.log)
+        .then(movies => {
+            let html = "";
+            movies.forEach(function (element) {
+                html +=
+                    "<div>" + element.title + "</div>" +
+                    "<div>" + element.rating + "</div>";
+                $("#movie-container").html(html);
+            })
         })
+}
+displayMovies();
+
+const ratingButtons = document.getElementsByName('rating');
+let newMovieRating
+
+function runLoop() {
+    ratingButtons.forEach(element => {
+        if (element.checked) {
+            console.log(element.value)
+            newMovieRating = element.value;
+        }
     })
+}
+runLoop();
 
 
 
-// function addMovie(){
-//     e.preventDefault()
-//     console.log("Add movie button works!")
-// }
+const addMovieButton = $("#add-movie-btn");
+const newMovieTitle = $("#movie-title-input").val();
 
-// $("#add-movie-btn").click(addMovie);
+
+function addMovie(e){
+    e.preventDefault();
+    console.log("Add movie button works!")
+    const newMovie = {title: newMovieTitle, rating: 5};
+    const url = "https://chestnut-endurable-ixia.glitch.me/movies";
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newMovie),
+    };
+    fetch(url, options)
+        .then(displayMovies)
+        .catch(alert("Post request rejected."));
+}
+
+addMovieButton.click(addMovie);
 
